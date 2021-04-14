4e8797050f03f89125596ba33c38f0739f6b4a03
AgilityCIS <carlosb@agilitycis.com>
2021-04-14 12:59:14 -0400

    Basic examples from following w3school nodejs basic tutorial

---
+++ firstExample/index.js
@@ -0,0 +1,92 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    let q = url.parse(req.url, true).query;
+//    let txt = q.year + ' ' + q.month;
+//    res.end(txt);
+//}).listen(8080);
+
+//fs examples
+//http.createServer(function (req, res) {
+//    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+//        if (err) throw err;
+//        console.log('Saved!');
+//    });
+//
+//    fs.open('mynewfile2.txt', 'w', function (err, file) {
+//        if (err) throw err;
+//        console.log('Saved!');
+//    });
+//
+//    try {
+//        fs.readFile('demofile1.html', function (err, data) {
+//            if (!err) {
+//                res.writeHead(200, { 'Content-Type': 'text/html' });
+//                res.write(data);
+//            } else {
+//                console.log(err);
+//            }
+//            return res.end();
+//        });
+//    } catch (error) {
+//        console.log(error);
+//    }
+//}).listen(8080);
+
+//url examples
+//let adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+//let q = url.parse(adr, true);
+//
+//console.log(q.host); //returns 'localhost:8080'
+//console.log(q.pathname); //returns '/default.htm'
+//console.log(q.search); //returns '?year=2017&month=february'
+//
+//let qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+//console.log(qdata.month); //returns 'february'
+
+//http.createServer(function (req, res) {
+//    var q = url.parse(req.url, true);
+//    var filename = '.' + q.pathname;
+//    fs.readFile(filename, function (err, data) {
+//        if (err) {
+//            res.writeHead(404, { 'Content-Type': 'text/html' });
+//            return res.end('404 Not Found');
+//        }
+//        res.writeHead(200, { 'Content-Type': 'text/html' });
+//        res.write(data);
+//        return res.end();
+//    });
+//}).listen(8080);
+
+//package use example
+
+//let uc = require('upper-case');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.write(uc.upperCase('Hello World!'));
+//    res.end();
+//}).listen(8080//
+
+//events examples
+
+let rs = fs.createReadStream('./demofile1.html');
+rs.on('open', function () {
+    console.log('The file is open');
+});
+
+let events = require('events');
+let eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+};
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
